d72364b9f002943120c08e4d8cad9ac1feb56d1f
郑康霖 <1255964530@qq.com>
2018-12-06 16:28:05 +0800

    styled-components组件升级v4版本后移除injextGlobal API, 改成createGlobalStyle API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Header from './common/header'
+import { GlobalStyle } from './style'
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <Fragment>
+        <GlobalStyle />
+        <Header />
+      </Fragment>
     )
   }
 }
--- src/style.js
+++ src/style.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
-createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -45,3 +45,5 @@ createGlobalStyle`
     border-spacing: 0;
   }
 `
+
+export default GlobalStyle
