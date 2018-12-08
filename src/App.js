e545b5e22159e5b5cc7ebb6abaaf958a6bdd4c8f
郑康霖 <1255964530@qq.com>
2018-12-08 16:47:30 +0800

    初始化store的代码

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component, Fragment } from 'react'
-import Header from './common/header'
 import { GlobalStyle } from './style'
 import { GlobalIconfont } from './statics/iconfont/iconfont'
+import { Provider } from 'react-redux'
+import store from './store'
+import Header from './common/header'
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <GlobalStyle />
-        <GlobalIconfont />
-        <Header />
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <GlobalStyle />
+          <GlobalIconfont />
+          <Header />
+        </Fragment>
+      </Provider>
     )
   }
 }
