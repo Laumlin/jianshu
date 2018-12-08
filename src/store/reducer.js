cf62353f3751eb487e6432b4d9b0b0e42ae04afb
郑康霖 <1255964530@qq.com>
2018-12-08 17:23:43 +0800

    将state相关的代码集中到store管理

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,17 @@
-const defaultState = {}
+const defaultState = {
+  focused: false
+}
 
 export default (state = defaultState, action) => {
+  if (action.type === 'handleInputFocus') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.focused = true
+    return newState
+  }
+  if (action.type === 'handleInputBlur') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.focused = false
+    return newState
+  }
   return state
 }
