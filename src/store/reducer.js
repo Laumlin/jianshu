4df355b665a10410f1a1155418dad5dffc86f327
郑康霖 <1255964530@qq.com>
2018-12-08 23:21:32 +0800

    Header组件store新建index.js作为该目录的输出口

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import HeaderReducer from '../common/header/store/reducer'
+import HeaderReducer from '../common/header/store/'
 
 export default combineReducers({
   header: HeaderReducer
