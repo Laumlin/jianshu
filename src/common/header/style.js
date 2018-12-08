ed0e319f05dabe2e4913654c4ed74b3f3fac1198
郑康霖 <1255964530@qq.com>
2018-12-08 16:31:09 +0800

    调整search框的css代码的位置

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -51,18 +51,6 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
   float: left;
   position: relative;
-  .slide-enter {
-    transition: all 0.2s ease-out;
-  }
-  .slide-enter-active {
-    width: 240px;
-  }
-  .slide-exit {
-    transition: all 0.2s ease-out;
-  }
-  .slide-exit-active {
-    width: 160px;
-  }
   .iconfont {
     position: absolute;
     right: 5px;
@@ -98,6 +86,18 @@ export const NavSearch = styled.input.attrs({
 
   &.focused {
     width: 240px;
+  }
+  &.slide-enter {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-exit-active {
+    width: 160px;
   }
 `
 
