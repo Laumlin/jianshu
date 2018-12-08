23be4565a708294c3c0258712554ba5966edeacf
郑康霖 <1255964530@qq.com>
2018-12-08 10:31:13 +0800

    导航栏middle的内容：首页、下载App、登录、Aa

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { 
   HeaderWrapper,
-  Logo
+  Logo,
+  Nav,
+  NavItem
 } from './style'
 
 class Header extends Component {
@@ -9,6 +11,12 @@ class Header extends Component {
     return (
       <HeaderWrapper>
         <Logo></Logo>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavItem className="right">登录</NavItem>
+        </Nav>
       </HeaderWrapper>
     )
   }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -13,9 +13,35 @@ export const Logo = styled.a.attrs({
   positon: absolute;
   top: 0;
   left: 0;
-  display: block;
+  display: inline-block;
   width: 100px;
   height: 58px;
   background: url(${logoImg});
   background-size: contain;
+`
+
+export const Nav = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  margin-left: -480px;
+  width: 960px;
+  height: 100%;
+`
+
+export const NavItem = styled.div`
+  line-height: 58px;
+  color: #333;
+  padding: 0 15px;
+  font-size: 17px;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right; 
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+
 `
