e8a6b2e2d35e143aea7654c90891311debb3db8a
郑康霖 <1255964530@qq.com>
2018-12-09 10:23:48 +0800

    使用redux-immutable统一数据格式，redux-immutable:将根目录的state也变成immutable对象

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -50,7 +50,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.get('focused')
+    focused: state.getIn(['header', 'focused'])
+    // 等价于 focused: state.get('header').get('focused')
   }
 }
 
