0c1b693430c12fcd91efad4a40eb7845b60d3e0e
郑康霖 <1255964530@qq.com>
2018-12-09 23:33:21 +0800

    新增home和detail两个路由页面

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component, Fragment } from 'react'
 import { GlobalStyle } from './style'
 import { GlobalIconfont } from './statics/iconfont/iconfont'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import store from './store'
 import Header from './common/header'
+import Detail from './pages/detail'
+import Home from './pages/home'
 
 class App extends Component {
   render() {
@@ -13,6 +16,12 @@ class App extends Component {
           <GlobalStyle />
           <GlobalIconfont />
           <Header />
+          <BrowserRouter>
+            <Fragment>
+              <Route path='/' exact component={Home}></Route>
+              <Route path='/detail' exact component={Detail}></Route>
+            </Fragment>
+          </BrowserRouter>
         </Fragment>
       </Provider>
     )
