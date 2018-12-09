3139ae541b05f0077c02060dfab55bdf9518d59a
郑康霖 <1255964530@qq.com>
2018-12-09 11:30:04 +0800

    由于体积和逻辑越来越多，将Header组件恢复为有状态组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
 import { actionCreators } from './store/'
@@ -18,63 +18,64 @@ import {
   Button
 } from './style'
 
-const getListArea = (show) => {
-  if (show) {
-    return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一批</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
-    )
-  } else {
-    return null
+class Header extends Component {
+  getListArea(show) {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null
+    }
   }
-}
-
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-    <Logo></Logo>
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载App</NavItem>
-        <SearchWrapper>
-          <CSSTransition
-            timeout={200}
-            in={props.focused}
-            classNames="slide">
-            <NavSearch 
-              className={props.focused ? 'focused' : ''}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe60d;</i>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">
-          <i className="iconfont">&#xe636;</i>
-        </NavItem>
-      </Nav>
-      <Addition>
-        <Button className='reg'>注册</Button>
-        <Button className='write'>
-          <i className="iconfont">&#xe617;</i>
-        写文章</Button>
-      </Addition>
-    </HeaderWrapper>    
-  )
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo></Logo>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              timeout={200}
+              in={this.props.focused}
+              classNames="slide">
+              <NavSearch 
+                className={this.props.focused ? 'focused' : ''}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe60d;</i>
+            {this.getListArea(this.props.focused)}
+          </SearchWrapper>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+        </Nav>
+        <Addition>
+          <Button className='reg'>注册</Button>
+          <Button className='write'>
+            <i className="iconfont">&#xe617;</i>
+          写文章</Button>
+        </Addition>
+      </HeaderWrapper>    
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
