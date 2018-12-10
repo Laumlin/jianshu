f4cc783e95878f538c6b04c7c30a1b6e073fc004
郑康霖 <1255964530@qq.com>
2018-12-10 23:09:57 +0800

    home组件的reducer导出到根目录下的reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux-immutable'
 import { reducer as HeaderReducer } from '../common/header/store/'
+import { reducer as HomeReducer } from '../pages/home/store'
 
 export default combineReducers({
-  header: HeaderReducer
+  header: HeaderReducer,
+  home: HomeReducer,
 })
