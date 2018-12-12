416799305f3980254b092f8e1dbd3dd2ded6140d
郑康霖 <1255964530@qq.com>
2018-12-12 21:37:00 +0800

    home主页--优化代码reducer.js

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -8,19 +8,25 @@ const defaultState = fromJS({
   page: 1
 })
 
+const getHomeData = (state, action) => {
+  return state.merge({
+    'articleList': fromJS(action.articleList),
+    'authorList': fromJS(action.authorList),
+    'recommendList': fromJS(action.recommendList)
+  })
+}
+const getMoreArticleList = (state, action) => {
+  return state.merge({
+    'articleList': state.get('articleList').concat(fromJS(action.articleList)),
+    'page': action.page
+  })  
+}
 export default (state = defaultState, action) => {
   switch (action.type) {
     case contains.HOME_INIT_DATA:
-      return state.merge({
-        'articleList': fromJS(action.articleList),
-        'authorList': fromJS(action.authorList),
-        'recommendList': fromJS(action.recommendList)
-      })
+      return getHomeData(state, action)
     case contains.MORE_ARTICLE_LIST: 
-      return state.merge({
-        'articleList': state.get('articleList').concat(fromJS(action.articleList)),
-        'page': action.page
-      })  
+      return getMoreArticleList(state, action)
     default:
       return state
   }
