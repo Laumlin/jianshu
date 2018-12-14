6fcb24309290a168a25bd3e7c3ea47ea8d6c2da0
郑康霖 <1255964530@qq.com>
2018-12-14 09:17:12 +0800

    detail页面：detail的store注入到根目录的store中

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux-immutable'
 import { reducer as HeaderReducer } from '../common/header/store/'
 import { reducer as HomeReducer } from '../pages/home/store'
 import { reducer as ToTopReducer } from '../common/toTop/store'
+import { reducer as DetailReducer } from '../pages/detail/store'
 
 export default combineReducers({
   header: HeaderReducer,
   home: HomeReducer,
   toTop: ToTopReducer,
+  detail: DetailReducer,
 })
