8046e40a82bcc131a0daa9849a194b77fdbf9077
郑康霖 <1255964530@qq.com>
2018-12-15 21:17:06 +0800

    登录页面: 初始化store

---
+++ src/pages/login/store/reducer.js
@@ -0,0 +1,12 @@
+import { fromJS } from 'immutable'
+
+const defaultState = fromJS({
+  login: false
+})
+
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,12 @@ import { reducer as HeaderReducer } from '../common/header/store/'
 import { reducer as HomeReducer } from '../pages/home/store'
 import { reducer as ToTopReducer } from '../common/toTop/store'
 import { reducer as DetailReducer } from '../pages/detail/store'
+import { reducer as LoginReducer } from '../pages/login/store'
 
 export default combineReducers({
   header: HeaderReducer,
   home: HomeReducer,
   toTop: ToTopReducer,
   detail: DetailReducer,
+  login: LoginReducer,
 })
