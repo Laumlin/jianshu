63915c8933a252c3043e8e14cf18e8fdabbe6252
郑康霖 <1255964530@qq.com>
2018-12-17 14:42:59 +0800

    文章详情页面：fix 由于App.js中/detail的router对应的component变成了loadable.js, 所以detail组件index.js无法直接获取到router的params等路由信息，要使用withRouter方法解决

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { actionCreators } from './store'
+import { withRouter } from 'react-router-dom'
 import { DetailWrapper, Header, Content } from './style'
 
 class Detail extends PureComponent {
@@ -36,4 +37,4 @@ const mapDispatch = (dispatch) => {
     }
   }
 }
-export default connect(mapState, mapDispatch)(Detail)
+export default connect(mapState, mapDispatch)(withRouter(Detail))
